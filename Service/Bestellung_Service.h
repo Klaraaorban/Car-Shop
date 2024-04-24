cbb7f49e06418b4deb044f61e4ace31d74c75f8d
Raul1963 <raulmarginean1963@gmail.com>
2024-04-24 19:47:54 +0300

    Commit 2

---
+++ Service/Bestellung_Service.h
@@ -0,0 +1,14 @@
+//
+// Created by raulm on 4/24/2024.
+//
+
+#ifndef CARSHOP_BESTELLUNG_SERVICE_H
+#define CARSHOP_BESTELLUNG_SERVICE_H
+
+
+class Bestellung_Service {
+
+};
+
+
+#endif //CARSHOP_BESTELLUNG_SERVICE_H
