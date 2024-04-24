0127fb94b29601cb066b0106850a6a11444a49ac
Molnar Bence <molnarbence2014@gmail.com>
2024-04-24 19:56:40 +0300

    added to directory

---
+++ Domain/customerDomain.h
@@ -0,0 +1,69 @@
+#ifndef CAR_SHARING_CUSTOMER_DOMAIN_H
+#define CAR_SHARING_CUSTOMER_DOMAIN_H
+#include <string>
+
+struct Address{
+    std::string country;
+    std::string city;
+    std::string street;
+    int streetNumber;
+};
+
+struct E_mail{
+    std::string mailAddress;
+    std::string mailPassword;
+};
+
+struct CustomerName{
+    std::string lastName;
+    std::string firstName;
+};
+class customer {
+
+private:
+    int customerID;
+    CustomerName customerName;
+    E_mail customerMail;
+    Address customerAddress;
+    std::string customerPhoneNr;
+    std::string customerNote; //something special about the customer
+    bool GdprDeleted;          //if true -- costumer dates private
+
+
+
+public:
+    customer(CustomerName customerName,E_mail customerMail,Address costumerAddress,
+             std::string customerPhoneNr,std::string customerNote,bool GdprDeleted);
+
+    int getCustomerID() const;
+
+    void setCustomerID(int customerId);
+
+    const CustomerName &getCustomerName() const;
+
+    void setCustomerName(const CustomerName &customerName);
+
+    const E_mail &getCustomerMail() const;
+
+    void setCustomerMail(const E_mail &customerMail);
+
+    const Address &getCustomerAddress() const;
+
+    void setCustomerAddress(const Address &costumerAddress);
+
+    const std::string &getCustomerPhoneNr() const;
+
+    void setCustomerPhoneNr(const std::string &customerPhoneNr);
+
+    const std::string &getCustomerNote() const;
+
+    void setCustomerNote(const std::string &customerNote);
+
+    bool isGdprDeleted() const;
+
+    void setGdprDeleted(bool gdprDeleted);
+
+};
+
+
+#endif //CAR_SHARING_CUSTOMER_DOMAIN_H
