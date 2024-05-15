1032d019db2113b5a264a93af7fb810e4ca8d07e
Klaraaorban <klara.orban@stud.ubbcluj.ro>
2024-05-15 19:50:50 +0300

    updated 'saveJson' function

--- CustomerService/customer_service.cpp
+++ CustomerService/customer_service.cpp
@@ -4,20 +4,38 @@ customer_service::customer_service(const std::string &path) : path(path) {
     loadJson();
 }
 
-//void customer_service::loadJson() {
-//    ifstream file(path);
-//    if(file.is_open()){
-//        json x;
-//        file >> x;
-//        for(const auto &i : x){
-//            customer cus(
-//                    i["customerID"], i["customerName"], i["customerMail"],
-//                    i["customerAddress"], i["customerPhoneNr"], i["customerNote"]);
-//            customers.push_back(cus);
-//        }
-//        file.close();
-//    }
-//}
+void customer_service::loadJson() {
+    ifstream file(path);
+    if(file.is_open()){
+        json x;
+        file >> x;
+        for(const auto &i : x){
+            auto n = i["customerName"];
+            auto token = n.dump();
+            CustomerName newStruct;
+            newStruct.firstName = token.substr(0,token.find(" "));
+            newStruct.lastName = token.substr(1,token.find(" "));
+            auto m = i["customerMail"];
+            auto tok_m = m.dump();
+            E_mail newMail;
+            newMail.mailAddress = tok_m.substr(0,tok_m.find(" "));
+            newMail.mailPassword = tok_m.substr(1,tok_m.find(" "));
+
+            auto a = i["customerAddress"];
+            auto tok_a = a.dump();
+            Address newAddress;
+            newAddress.country = tok_a.substr(0,tok_a.find(" "));
+            newAddress.city = tok_a.substr(1,tok_a.find(" "));
+            newAddress.street = tok_a.substr(2,tok_a.find(" "));
+            newAddress.streetNumber = stoi(tok_a.substr(3,tok_a.find(" ")));
+            customer cus(
+                    newStruct, newMail,
+                    newAddress, i["customerPhoneNr"], i["customerNote"], 1);
+            customers.push_back(cus);
+        }
+        file.close();
+    }
+}
 
 void customer_service::saveJson() const {
     json x = json::array();
