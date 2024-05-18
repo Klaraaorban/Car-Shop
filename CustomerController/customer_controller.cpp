8420431acc66ff3eb68506f3e4d0b1cfcea3289f
NagyBalazsMark <nagybalazsmark@gmail.com>
2024-05-18 11:51:05 +0300

    git try

---
+++ CustomerController/customer_controller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nagyb on 5/18/2024.
+//
+
+#include "customer_controller.h"
---
+++ CustomerController/customer_controller.h
@@ -0,0 +1,16 @@
+//
+// Created by nagyb on 5/18/2024.
+//
+
+#ifndef CARSHOP_CUSTOMER_CONTROLLER_H
+#define CARSHOP_CUSTOMER_CONTROLLER_H
+
+
+class customer_controller {
+private:
+    int i;
+
+};
+
+
+#endif //CARSHOP_CUSTOMER_CONTROLLER_H
