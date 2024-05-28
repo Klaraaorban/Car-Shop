950e84c4d5fad51ce3d28a0b0a16389f3fcfe801
Molnar Bence <molnarbence2014@gmail.com>
2024-05-28 15:08:14 +0300

    added customer controller from private branch

--- Controllers/customerController.h
+++ Controllers/customerController.h
@@ -5,35 +5,31 @@
 #ifndef CARSHOP_CUSTOMERCONTROLLER_H
 #define CARSHOP_CUSTOMERCONTROLLER_H
 
-#include "../Services/customerService.h"
+#include "../CustomerService/customer_service.h"
 
 class customerController {
 private:
-    customerService &customerService;
+    customerService &service;
 
     bool IsEmailUnique(const E_mail& email);                //finished
     bool IsEmailStructureCorrect(const E_mail& email);      //finished
     bool PhoneNumberFormatCorrect(std::string PhoneNR);     //finished
 
 public:
-    explicit customerController(class customerService &customerService);                             //finished
-    void CustomerAdd(CustomerName customerName,E_mail customerMail,Address costumerAddress,
-                     std::string customerPhoneNr,std::string customerNote,bool GdprDeleted);    //finished
-    void CustomerDelete(E_mail email);      //finished
-    void ModifyCustomer(E_mail old_email,CustomerName customerName,E_mail customerMail,Address costumerAddress,
-                        std::string customerPhoneNr,std::string customerNote,bool GdprDeleted); //finished
-    void CustomerAnonymisation(E_mail &email);          //finished
-    void CustomerByAuto();                              //TODO
-    std::vector<customer> ListAllCostumersSortedByFirstName();       // the cout will be in the UI  //finished
-    std::vector<customer> ListAllCostumersSortedByLastName();       // the cout will be in the UI  //finished
-    customer FindCustomerByEmail(const E_mail &email);                  //finished
-    customer FindCustomerByID(const int& id);                           //finished
-    customer FindCustomerByMobilePhone(const std::string& PhoneNr);     //finished
-    customer FindCustomerByName(CustomerName name);                     //finished
-    void ChangeCustomerEmailPassword(E_mail email,std::string newPassword);   // partially finished
-    void ChangeCustomerNote(E_mail email, std::string newNote);       // partially finished
+    explicit customerController(customerService &service);                             //finished
+    void customerAdd(CustomerName customerName, E_mail customerMail, Address costumerAddress,
+                     std::string customerPhoneNr, std::string customerNote, bool GdprDeleted, vector<int> favorites);    //finished
+    void customerDelete(std::string email);                                          //finished
+    customer FindCustomerByEmail(const E_mail &email);                          //finished
+    customer FindCustomerByID(const int& id);                                   //finished
+    customer FindCustomerByName(CustomerName name);                             //finished
+    customer FindCustomerByPhoneNr(const std::string& PhoneNr);                 //finished
+    std::vector<customer> ListAllCostumersSortedByName(std::string option);     //finished
+
+    void ChangeCustomer(std::string old_email, std::string option, E_mail mail, CustomerName name, Address address, std::string phoneNr,
+                        std::string note, bool GdprDeleted, std::vector<int> favorites);
     std::vector<int> SeeFavorites(E_mail &email);       //finished
-    void AddToFavorite(E_mail &email, int carID);       //finished
+    void AddToFavorite(E_mail email, int carID);        //finished
 
 };
 
